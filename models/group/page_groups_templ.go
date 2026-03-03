6cac96dbae6980c50d41dbd462423a2f217e2d05
Peter Szarvas <contact@peterszarvas.hu>
2026-03-03 10:52:13 +0100

    Add searchable field hints to table search forms

--- models/group/page_groups_templ.go
+++ models/group/page_groups_templ.go
@@ -132,7 +132,7 @@ func GroupsMain(data GroupsPageData) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = shared.TableSearchForm(utils.BuildTableSearchDatastarAction("/dashboard", utils.DefaultTablePageSize)).Render(ctx, templ_7745c5c3_Buffer)
+		templ_7745c5c3_Err = shared.TableSearchForm(utils.BuildTableSearchDatastarAction("/dashboard", utils.DefaultTablePageSize), "Searches: name, admin email").Render(ctx, templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
